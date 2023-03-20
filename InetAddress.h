097bfe6ddf51be657dc155bd5a323565051c5503
famous <bhpeng20@163.com>
2023-03-21 02:00:23 +0800

    first commit: IntAddress.h

---
+++ InetAddress.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string>
+
+namespace mymuduo
+{
+
+// 封装socket地址类型
+class InetAddress
+{
+public:
+    //本地环回
+    explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false);
+    // ip与port构造
+    InetAddress(const std::string& ip, uint16_t port);
+    //sockaddr构造
+    explicit InetAddress(const struct sockaddr_in& addr)
+    : addr_(addr)
+    { }
+
+    std::string toIp() const;
+    std::string toIpPort() const;
+    uint16_t toPort() const;
+
+    const struct sockaddr* getSockAddr() const
+    {   return reinterpret_cast<const struct sockaddr*>(&addr_); }
+
+    void setSockAddr(const sockaddr_in &addr) { addr_ = addr; };
+private:
+    struct sockaddr_in addr_;
+};
+
+}
